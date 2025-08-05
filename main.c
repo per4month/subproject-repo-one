16760471f2d1dc10e1eb025a65cea3d52f6bdf79
Maxim Vasilchenko <maksim.vasilchenko@eltex.loc>
2025-08-05 15:36:06 +0700

    3.10.0 add message about ice cube

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 
-/* hello comment */ /* UPD: comment*/
+/* hello comment */
 int main(int argc, char const *argv[])
 {
-    printf("[subproj-one] Hello, World!\n");
-    return 0;
+    printf("[subproj-one] I hate ice cube!\n");
+    return 1;
 }
